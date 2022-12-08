7f9b5ec70ee2ef1aaca0ca3c079f2e5be57558f1
VanJoost <117902025+VanJoost@users.noreply.github.com>
2022-12-08 16:03:30 +0700

    Add files via upload

---
+++ No.1_D_71220887.py
@@ -0,0 +1,10 @@
+
+bilangan = int(input("Masukan Bilangan : "))
+for i in range(1, 11):
+    hasil = bilangan * i
+    print(bilangan, "x", i, "=", hasil)
+
+bilangan = int(input("Masukan Bilangan : "))
+for i in range(50, 65):
+    hasil = bilangan / i
+    print(bilangan, ":", i, "=", hasil)
